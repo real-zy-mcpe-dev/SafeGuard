e1f9674001a51c3587719178bced5dbb766df7af
real-zy-mcpe-dev <taylorschoenberg@icloud.com>
2025-04-11 20:36:20 +0200

    Update config.js
    add owner password

--- scripts/config.js
+++ scripts/config.js
@@ -2,7 +2,7 @@
 export default {
     "version": "2.0.1",
     //the owner password to edit config in game
-    "OWNER_PASSWORD": "",
+    "OWNER_PASSWORD": "configbyrealzy772",
     "other":{
         //dev stuff
         "consoleDebugMode": true,
